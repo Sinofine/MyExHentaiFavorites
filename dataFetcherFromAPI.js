3ed6f1ef5e79481c414f93e7d55a9dcb07dbdae8
Sinofine <sinolot@outlook.com>
2019-08-27 22:40:05 +0800

    chore: update

---
+++ dataFetcherFromAPI.js
@@ -0,0 +1,15 @@
+const rawData = require("./dataFromFavorites.json");
+const fetch = require("node-fetch");
+let it = 0;
+let newData = [];
+(async function getData(){
+    newData = [...newData,...await (fetch("https://api.e-hentai.org/api.php",{method:"POST",body:JSON.stringify({
+        "method": "gdata",
+        "gidlist": rawData.slice(it,it+=20).map((val)=>{
+            let res = val.url.match(/https:\/\/exhentai.org\/g\/(.*)\/(.*)\//);
+            return [parseInt(res[1]),res[2]]
+        }),
+        "namespace": 1
+      })}).then(p=>p.json()).then(p=>p["gmetadata"]).then(p=>p.map(val=>({...val,url:`https://exhentai.org/g/${val.gid}/${val.token}/`}))))];
+      it<=rawData.length ? getData() : require("fs").writeFileSync("./dataFromAPI.json",JSON.stringify(newData));
+})();
