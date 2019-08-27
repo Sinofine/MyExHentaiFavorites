db5853f6f97623772c89b7fc53f378c1f865d767
Sinofine <sinolot@outlook.com>
2019-08-27 23:33:57 +0800

    chore: all clear

--- dataFetcherFromFavorites.js
+++ dataFetcherFromFavorites.js
@@ -5,7 +5,6 @@ let data = [];
 
 (async function getData(url){
     let content = await (fetch(url,{method:"GET",headers:{"Cookie":require("process").env["EX_COOKIE"]}}).then(data=>data.text()));
-    console.log(content);
     let res = EHParser.parseSearchPage((new JSDOM(content)).window.document);
     data = [...data,...(res.results)];
     if(res.next) 
