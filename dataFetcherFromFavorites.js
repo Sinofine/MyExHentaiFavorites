e818ea60bf88a0155e82955e5a595e91823511e8
Sinofine <sinolot@outlook.com>
2019-08-27 22:40:05 +0800

    chore: update

---
+++ dataFetcherFromFavorites.js
@@ -0,0 +1,14 @@
+const JSDOM = require("jsdom").JSDOM;
+const EHParser = require("eh-parser");
+const fetch = require("node-fetch");
+let data = [];
+
+(async function getData(url){
+    let content = await (fetch(url,{method:"GET",headers:{"Cookie":require("process").env["EX_COOKIE"]}}).then(data=>data.text()))
+    let res = EHParser.parseSearchPage((new JSDOM(content)).window.document);
+    data = [...data,...(res.results)];
+    if(res.next) 
+        getData(res.next)
+    else
+        require("fs").writeFileSync("./data.json",JSON.stringify(data));
+})("https://exhentai.org/favorites.php");
